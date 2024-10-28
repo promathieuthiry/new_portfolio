521466f0b82ea0c5e4d68ef35e845fa5b1af6180
Mathieu Thiry <promathieuthiry@gmail.com>
2024-10-28 11:59:38 +0100

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  site: "https://www.mathieuthiry.fr",
+  integrations: [tailwind(), sitemap()]
 });
