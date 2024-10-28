b52306269e35131b05025bb253177fd28734f5ed
Mathieu Thiry <promathieuthiry@gmail.com>
2024-10-28 17:25:14 +0100

    add rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,22 @@
+import rss from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    // The title, description and site URL are required
+    title: 'Mathieu Thiry Blog',
+    description: '"Stay updated with the latest projects and insights from Mathieu Thiry, a front-end developer specializing in React and TypeScript.',
+    site: context.site,
+    
+    // Items are your blog posts/articles
+    items: await getCollection('post').map((post) => ({
+      title: post.data.title,
+      pubDate: new Date(post.data.date),
+      description: post.data.description,
+      link: `/post/${post.slug}/`,
+      categories: post.data.tags,
+    })),
+    
+    // Optional custom XML
+    customData: `<language>en-us</language>`,
+  });
+}
