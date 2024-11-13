93198580dae627481e330b5e119a4c97f1fbd720
Mathieu Thiry <promathieuthiry@gmail.com>
2024-11-13 16:35:09 +0100

    install partytown for google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.mathieuthiry.fr",
-  integrations: [tailwind(), sitemap()]
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
